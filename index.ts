fb10ace1190bfb74e25e74237ac46f3f223a771c
ParkAsher <devasherpark@naver.com>
2023-01-31 03:27:07 +0900

    B반 7조 박현민

--- index.ts
+++ index.ts
@@ -3,13 +3,17 @@
   힌트: 타입도 변수처럼 type alias로 선언 가능.
 */
 
-type MyDiverseArray = {};
+type MyDiverseArray<T> = T[];
 
-const myDiverseArray = [true, 100, 101, 102, false];
-const anotherDiverseArray = [
-  myDiverseArray,
-  "backend developer",
-  { name: "hermione" },
+type Name = {
+    name: string;
+};
+
+const myDiverseArray: MyDiverseArray<Boolean | number> = [true, 100, 101, 102, false];
+const anotherDiverseArray: MyDiverseArray<typeof myDiverseArray | string | Name> = [
+    myDiverseArray,
+    'backend developer',
+    { name: 'hermione' },
 ];
 
 /*
@@ -17,10 +21,10 @@ const anotherDiverseArray = [
   stringOnlyArray에는 타입 인자를 전달하지 않아도 컴파일이 되도록 하세요.
 */
 
-type WithDefaultType = {};
+type WithDefaultType<T = string> = T[];
 
-const stringOnlyArray = ["we're", "all", "gonna", "make", "it"];
-const mixedArray = [2023, "year of the rabbit"];
+const stringOnlyArray: WithDefaultType = ["we're", 'all', 'gonna', 'make', 'it'];
+const mixedArray: WithDefaultType<number | string> = [2023, 'year of the rabbit'];
 
 /*
   3. createTupleTriplet이라는 함수가 있습니다.
@@ -28,7 +32,13 @@ const mixedArray = [2023, "year of the rabbit"];
   제네릭을 써서 함수를 완성하세요. (타입 변수, 인자 타입, 반환 타입)
 */
 
-export function createTupleTriplet(firstValue, secondValue, thirdValue) {}
+export function createTupleTriplet<T, U, V>(
+    firstValue: T,
+    secondValue: U,
+    thirdValue: V
+): [T, U, V] {
+    return [firstValue, secondValue, thirdValue];
+}
 
 /*
   4. strictCreateTupleTriplet이라는 함수가 있습니다.
@@ -36,19 +46,25 @@ export function createTupleTriplet(firstValue, secondValue, thirdValue) {}
   제네릭을 써서 함수를 완성하세요. (타입 변수, 인자 타입, 반환 타입)
 */
 
-export function strictCreateTupleTriplet(firstValue, secondValue, thirdValue) {}
+export function strictCreateTupleTriplet<T, U, V extends T>(
+    firstValue: T,
+    secondValue: U,
+    thirdValue: V[]
+): [T, U, V[]] {
+    return [firstValue, secondValue, thirdValue];
+}
 
 /*
   완성된 함수는 다음과같이 호출이 됐을 때 컴파일이 되거나 에러가 떠야됩니다.
 */
 
 // 컴파일
-strictCreateTupleTriplet("123", true, ["123"]); // ✅
+strictCreateTupleTriplet('123', true, ['123']); // ✅
 strictCreateTupleTriplet(2023, false, [2023]); // ✅
 
 // 에러 (진도를나갈 수 있도록 확인 후 주석처리하세요)
 strictCreateTupleTriplet(2023, false, [123]); // ❌ ([123] 부분 에러)
-strictCreateTupleTriplet("2023", false, ["i am a string"]); // ❌ (["i am a string"] 부분 에러)
+strictCreateTupleTriplet('2023', false, ['i am a string']); // ❌ (["i am a string"] 부분 에러)
 
 /*
   5. getRoleOptions와 getCuisineOptions라는 함수가 있습니다.
@@ -56,25 +72,43 @@ strictCreateTupleTriplet("2023", false, ["i am a string"]); // ❌ (["i am a str
 */
 
 enum Role {
-  ROLE_ADMIN = "ROLE_ADMIN",
-  ROLE_CUSTOMER = "ROLE_CUSTOMER",
-  ROLE_SELLER = "ROLE_SELLER",
+    ROLE_ADMIN = 'ROLE_ADMIN',
+    ROLE_CUSTOMER = 'ROLE_CUSTOMER',
+    ROLE_SELLER = 'ROLE_SELLER',
 }
 
 enum Cuisine {
-  CUISINE_KOREAN = "CUISINE_KOREAN",
-  CUISINE_ITALIAN = "CUISINE_ITALIAN",
-  CUISINE_THAI = "CUISINE_THAI",
+    CUISINE_KOREAN = 'CUISINE_KOREAN',
+    CUISINE_ITALIAN = 'CUISINE_ITALIAN',
+    CUISINE_THAI = 'CUISINE_THAI',
 }
 
 interface Option<T> {
-  label: string;
-  value: T;
+    label: string;
+    value: T;
 }
 
-export function getRoleOptions() {}
+export function getRoleOptions(): Option<Role>[] {
+    return Object.entries(Role).map(([label, value]) => {
+        let labelFirstChar: string = label.split('_')[1].charAt(0);
+        let labelOthers: string = label.split('_')[1].slice(1).toLowerCase();
+        return {
+            label: labelFirstChar + labelOthers,
+            value,
+        };
+    });
+}
 
-export function getCuisineOptions() {}
+export function getCuisineOptions(): Option<Cuisine>[] {
+    return Object.entries(Cuisine).map(([label, value]) => {
+        let labelFirstChar: string = label.split('_')[1].charAt(0);
+        let labelOthers: string = label.split('_')[1].slice(1).toLowerCase();
+        return {
+            label: labelFirstChar + labelOthers + ' food',
+            value,
+        };
+    });
+}
 
 /*
   6-7. Queue라는 class가 있습니다.
@@ -84,13 +118,27 @@ export function getCuisineOptions() {}
   힌트: 강의자료 Stack 참조하세요.
 */
 
-interface IQueue {
-  enqueue;
-  dequeue;
-  size;
+interface IQueue<T> {
+    enqueue(item: T): void;
+    dequeue(): T | undefined;
+    size(): number;
 }
 
-export class Queue {}
+export class Queue<T> implements IQueue<T> {
+    private queue: T[] = [];
+
+    enqueue(item: T): void {
+        this.queue.push(item);
+    }
+
+    dequeue(): T | undefined {
+        return this.queue.shift();
+    }
+
+    size(): number {
+        return this.queue.length;
+    }
+}
 
 /*
   8. IRepository라는 인터페이스가 있습니다.
@@ -98,11 +146,11 @@ export class Queue {}
   IRepository는 제네릭 인터페이스입니다.
 */
 
-interface IRepository {
-  create;
-  findById;
-  updateById;
-  deleteById;
+interface IRepository<T> {
+    create(item: T): void | Error;
+    findById(id: number): T | Error;
+    updateById(id: number): void | Error;
+    deleteById(id: number): void | Error;
 }
 
 /*
@@ -111,27 +159,30 @@ interface IRepository {
   길이를 알 수 없는 매개변수는 에러가납니다.
 */
 
-export function getLength(input) {}
+export function getLength<T extends string | number[]>(input: T): number {
+    return input.length;
+}
 
-getLength(123); // ❌
+// getLength(123); // ❌
 getLength([123]); // ✅
-getLength("12345"); // ✅
+getLength('12345'); // ✅
 
 /*
   10. EnumRecord 라는 타입이 있습니다.
   제네릭을 사용하여 myFirstRecord 그리고 mySecondRecord와 같은 객체를 충족하는 타입을 완성하세요.
 */
-
-type EnumRecord = {};
-
-const myFirstRecord = {
-  CUISINE_ITALIAN: ["pasta", "burrata"],
-  CUISINE_KOREAN: ["bibimbap", "kimchi"],
-  CUISINE_THAI: ["tom yum soup", "pad thai"],
+type EnumRecord<T extends Cuisine | Role> = {
+    [key in T]: string[];
 };
 
-const mySecondRecord = {
-  ROLE_ADMIN: ["payroll"],
-  ROLE_CUSTOMER: ["orders"],
-  ROLE_SELLER: ["products", "revenue"],
+const myFirstRecord: EnumRecord<Cuisine> = {
+    CUISINE_ITALIAN: ['pasta', 'burrata'],
+    CUISINE_KOREAN: ['bibimbap', 'kimchi'],
+    CUISINE_THAI: ['tom yum soup', 'pad thai'],
+};
+
+const mySecondRecord: EnumRecord<Role> = {
+    ROLE_ADMIN: ['payroll'],
+    ROLE_CUSTOMER: ['orders'],
+    ROLE_SELLER: ['products', 'revenue'],
 };
